54271c7da47884e90131b4cd65d774f750bc5697
ucoccto <edu1.ucoc@gmail.com>
2026-08-28 15:21:22 +0900

    Feat: 인프라 수정

--- infra/opensearch.tf
+++ infra/opensearch.tf
@@ -85,11 +85,11 @@ resource "aws_opensearch_domain" "factory" {
   # 데이터 구조가 변경되면 => 교체해야함
   # 이런 정보가 없으면 opensearch가 데이터를 보고 => 타입 추정 => 타입 매칭 (오류 많이 발생함)
   # timestamp 에서 오류 발생이 많음
-  provisioner "local-exec" {
-    command = <<-EOT
-      curl -X PUT "https://${self.endpoint}/factory-sensor" \
-        -H "Content-Type: application/json" \
-        -d '{"mappings":{"properties":{"@timestamp":{"type":"date_nanos"},"timestamp":{"type":"date"},"vector_ingest_at":{"type":"date_nanos"},"sensor_id":{"type":"keyword"},"temperature":{"type":"float"},"humidity":{"type":"float"},"status":{"type":"keyword"}}}}'
-    EOT
-  }
+  # provisioner "local-exec" {
+  #   command = <<-EOT
+  #     curl -X PUT "https://${self.endpoint}/factory-sensor" \
+  #       -H "Content-Type: application/json" \
+  #       -d '{"mappings":{"properties":{"@timestamp":{"type":"date_nanos"},"timestamp":{"type":"date"},"vector_ingest_at":{"type":"date_nanos"},"sensor_id":{"type":"keyword"},"temperature":{"type":"float"},"humidity":{"type":"float"},"status":{"type":"keyword"}}}}'
+  #   EOT
+  # }
 }

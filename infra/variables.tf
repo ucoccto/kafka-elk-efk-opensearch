6d926b40c5e5b9d86330f0ada816f965675161e7
ucoccto <edu1.ucoc@gmail.com>
2026-08-28 13:35:10 +0900

    Feat: 변수 세팅할 항목 설정

--- infra/variables.tf
+++ infra/variables.tf
@@ -13,8 +13,16 @@ variable "project_name" {
 variable "allowed_cidr" {
   description = "opensearch 대시보드/API에 접속한 공인 IP x.x.x.x/32"
   type        = string
+  # 접속 위치가 바뀌면 접근 x => ip를 변경하여 인프라 반영시켜야 함
+  default     = "222.108.125.33/32"
   validation {
-    condition = can(cidrhost(var.allowed_cidr, 0))
+    condition     = can(cidrhost(var.allowed_cidr, 0))
     error_message = "가능한 주소는 CIDR 형식이여야 합니다."
   }
 }
+
+# opensearch, spec(버전, 인스턴유형, 볼륨단위, 인덱스등 설정)
+
+# firhose 이름, firhose->opensearch : iam role name
+
+# vector -> firhose : iam role name

f0d4008ca466327439b2651f3e7a2123b908a87b
ucoccto <edu1.ucoc@gmail.com>
2026-08-31 13:34:36 +0900

    Feat: sfn 코멘트

--- infra/stepfunctions.tf
+++ infra/stepfunctions.tf
@@ -17,16 +17,19 @@ resource "aws_sfn_state_machine" "pipeline" {
     Comment = "EventBridge -> Stepfunctions B/S/G pipeline"
     # 진행 방향, 어떤 Task(or State)부터 시작하는가?
     StartAt = "CheckBronze"
-    # 전체 State 목록 구성
+    # 전체 State 목록 구성 (분기 과정을 통해서 시나리오에 맞춰 task가 처리)
     States = {
         # 람다 함수를 통해서 브론즈 값 체크, 설정값등 처리
-        CheckBronze = {}
-        CheckBronze = {}
-        CheckBronze = {}
-        CheckBronze = {}
-        CheckBronze = {}
-        CheckBronze = {}
-        CheckBronze = {}
+        CheckBronze  = {}
+        # 전단계에서 람다 함수 호출 => 결과를 기반 실제 존재하는지 체크
+        BronzeExists = {}
+        # 데이터가 없어서 처리해하는 설정
+        NoBronzData  = {}
+        # Glue ETL Job을 이용하여 spark를 통해서 데이터를 일괄 처리
+        BronzeToSilver = {}
+        #CheckBronze = {}
+        #CheckBronze = {}
+        #CheckBronze = {}
     }
 
   })

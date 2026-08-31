1cd97ea4d997b485b63c2c4e2120b72a62f2f459
ucoccto <edu1.ucoc@gmail.com>
2026-08-31 10:38:28 +0900

    Feat: s3, firehose 적용 수정중

--- infra/firehose.tf
+++ infra/firehose.tf
@@ -1,7 +1,22 @@
 # vector -> 데이터 put -> Firehose 입력 (direct put firehose)
-resource "aws_kinesis_firehose_delivery_stream" "opensearch" {
-  name        = var.project_name
-  destination = "opensearch"
+resource "aws_kinesis_firehose_delivery_stream" "bronze" {
+  name        = local.firehose_name
+  # 목적지 수정
+  destination = "extended_s3"
+  # 목적지 구성
+  extended_s3_configuration {
+    # role
+    role_arn = aws_iam_role.firehose.arn
+    # 버킷
+    bucket_arn = aws_s3_bucket.data_lake.arn
+    # 버퍼크기
+    # 버퍼인터벌
+    # 압축형태
+    # 프리픽스
+    # 에러플릭
+    # 로그->클라우드와치
+  }
+
 
   depends_on = [
     aws_iam_role_policy.firehose
